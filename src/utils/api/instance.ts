4cadeb87a9507887ff9b916ca404ac892bd7c920
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-06-23 17:41:53 +0500

    change api url

--- src/utils/api/instance.ts
+++ src/utils/api/instance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const $api = axios.create({
-  baseURL: 'http://176.117.79.51:8090/api/v1',
+  baseURL: 'http://classbox.uz/api/v1',
   withCredentials: true,
 });
